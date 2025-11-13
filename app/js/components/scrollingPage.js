818406fdbcfd6ca1db61a3a5e5da15d791de0e0b
Sivukha <l1lkost@yandex.ru>
2025-11-13 17:46:00 +0300

    main slider fixed

--- app/js/components/scrollingPage.js
+++ app/js/components/scrollingPage.js
@@ -1,12 +1,10 @@
-export const scrollingPage = () =>{
+export const scrollingPage = () => {
     const header = document.querySelector(".header"),
         mainNode = document.querySelector(".main"),
         mainMenu = document.querySelector(".mobile-menu")
 
-    if(header && mainNode) {
-        setTimeout(()=>{
-            mainNode.style.setProperty("--top", `${header.clientHeight}px`);
-            mainMenu.style.setProperty("--top", `${header.clientHeight}px`);
-        }, 200);
+    if (header && mainNode) {
+        mainNode.style.setProperty("--top", `${header.clientHeight}px`);
+        mainMenu.style.setProperty("--top", `${header.clientHeight}px`);
     };
 }
